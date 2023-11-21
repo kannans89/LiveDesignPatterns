d433737cfbe5f4951caae77a2bb7e3ae1a6887d6
kannan <kannan.mumbai@yahoo.com>
2023-11-21 13:38:05 +0530

    book

---
+++ Day1/LSPSln/LSPViolationApp/Model/Square.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSPViolationApp.Model
+{
+    public class Square : Rectangle
+    {
+        public Square(int side) : base(side, side)
+        {
+        }
+
+        public override void SetHeight(int pheight)
+        {
+            _width=_height=pheight;
+        }
+
+        public override void SetWidth(int pheight)
+        {
+            _width = _height = pheight;
+        }
+    }
+}
